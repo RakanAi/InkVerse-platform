93f69fb171cadb643c6cc4a4852d2a72b13c257c
Rakan Odeh <rakanodah@gmail.com>
2026-01-23 02:03:36 +0300

    github test

--- inkverse-frontend/src/main.jsx
+++ inkverse-frontend/src/main.jsx
@@ -1,20 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import {AuthProvider} from './Context/AuthProvider.jsx';
+import { AuthProvider } from "./Context/AuthProvider.jsx";
+import { HashRouter } from "react-router-dom";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="*" element={<App />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="*" element={<App />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
-)
+);
